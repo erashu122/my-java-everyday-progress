23773fd72b87c83c6242b7b1dddf527773726853
erashu122 <kbitu8676@gmail.com>
2025-02-24 21:04:05 +0530

    initial commit

--- Array/array1.java
+++ Array/array1.java
@@ -1,7 +1,27 @@
 public class array1 {
     public static void main(String[] args) {
-       int x= new int[1,2,3,4,5];
-          
+    //   int [][] x=new int [3][2]; //[[I
+    //   double[] y=new double[2];  //[D
+    //   short[] z=new short[2];    //[S
+    //   byte [] a=new byte[2];     //[B
+    //   boolean[] b=new boolean[3];//[Z
+    //   System.out.println(x.getClass().getName());
+    //   System.out.println(y.getClass().getName());
+    //   System.out.println(z.getClass().getName());
+    //   System.out.println(a.getClass().getName());
+    //   System.out.println(b.getClass().getName());
+    //   int [] c=new int[-3]; //vailid
+    //   System.out.println(c.getClass().getName());
+    //   int[] d=new int['a']; //vailid 
+    //   System.out.println(d.getClass());
+    // byte b=20;
+    // int[] x=new int[b];//vailid
+    // short s=30;
+    // int[] x=new int[s];
+    // System.out.println(x.getClass().getName());
+    // int [] b=new int[10];
+    // System.out.println(b.getClass().getName());
+    int[] x=new int[2147483648];
     }
     
-}
+} 
