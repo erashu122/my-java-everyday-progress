7c99c3b7bc4e6fa80c25a1c2d27c9df1ed1a8244
erashu122 <kbitu8676@gmail.com>
2025-02-24 23:57:12 +0530

    initial commit

--- Array/array2.java
+++ Array/array2.java
@@ -1,28 +1,30 @@
 public class array2 {
     public static void main(String[] args) {
-    int[][] x=new  int[2][];
-    x[0]=new int[2];      //2d array
-    x[1]=new int[3];
+    // int[][] x=new  int[2][];
+    // x[0]=new int[2];      //2d array
+    // x[1]=new int[3];
 
-    int [][][] x=new int[2][][]; //3Darray
-    x[0]=new int[3][];
-    x[0][0]=new int[1];
-    x[0][2]=new int[3];
-    x[1]=new int[2][2];
-    x[1][0]=new int[2];
-    x[1][1]=new int[2];
+    // int [][][] x=new int[2][][]; //3Darray
+    // x[0]=new int[3][];
+    // x[0][0]=new int[1];
+    // x[0][2]=new int[3];
+    // x[1]=new int[2][2];
+    // x[1][0]=new int[2];
+    // x[1][1]=new int[2];
 
-    check valid array or not
-    int[] a=new int[]; //error: array dimension missing
-    int[] a=new int[3];  //no compile time error
-    int[][] a=new int[][]; //error: array dimension missing
-    int[][] a=new int[][4];// error: ']' expected
-    int[][] a=new int[3][];  //vailid no compile time error
-    int[][] a=new int[3][4];  // vailid,no comile time error
-    int[][][] a=new int[3][4][5]; //vailid ,no comile time error 
-    int[][][] a=new int[3][4][]; //vailid
-    int[][][] a=new int[3][][5]; //']' expected
-    int[][][] a=new int[][4][5];   //']' expected
+    //check valid array or not
+    //int[] a=new int[]; //error: array dimension missing
+    //int[] a=new int[3];  //no compile time error
+    //int[][] a=new int[][]; //error: array dimension missing
+    //int[][] a=new int[][4];// error: ']' expected
+    //int[][] a=new int[3][];  //vailid no compile time error
+    //int[][] a=new int[3][4];  // vailid,no comile time error
+    //int[][][] a=new int[3][4][5]; //vailid ,no comile time error 
+    //int[][][] a=new int[3][4][]; //vailid
+    //int[][][] a=new int[3][][5]; //']' expected
+    //int[][][] a=new int[][4][5];   //']' expected
+    System.out.println(a);
+
             
     }
    
