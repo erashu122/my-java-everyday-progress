ae820128e3a386bbb04df223063a508a98acf3a6
erashu122 <kbitu8676@gmail.com>
2025-02-24 23:19:36 +0530

    initial commit

--- Array/array2.java
+++ Array/array2.java
@@ -1,8 +1,16 @@
 public class array2 {
     public static void main(String[] args) {
-    int[][] x=new  int[2][];
-    x[0]=new int[2];
-    x[1]=new int[3];
+    // int[][] x=new  int[2][];
+    // x[0]=new int[2];      //2d array
+    // x[1]=new int[3];
+
+    int [][][] x=new int[2][][];
+    x[0]=new int[3][];
+    x[0][0]=new int[1];
+    x[0][2]=new int[3];
+    x[1]=new int[2][2];
+    x[1][0]=new int[2];
+    x[1][1]=new int[2];
             
     }
    
