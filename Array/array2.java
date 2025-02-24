b22f02002c8b1d5cfb739ee78132c8c3da89a420
erashu122 <kbitu8676@gmail.com>
2025-02-24 22:45:19 +0530

    initial commit

--- Array/array2.java
+++ Array/array2.java
@@ -1,4 +1,10 @@
 public class array2 {
-    
+    public static void main(String[] args) {
+    int[][] x=new  int[2][];
+    x[0]=new int[2];
+    x[1]=new int[3];
+            
+    }
+   
     
 }
