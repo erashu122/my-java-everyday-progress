b22e5e3f96b131cae79fad308dd74aac054ce783
erashu122 <kbitu8676@gmail.com>
2025-02-24 21:00:11 +0530

    Add initial implementation of array1 class and update day5 class to demonstrate double variable usage

---
+++ Array/array1.java
@@ -0,0 +1,7 @@
+public class array1 {
+    public static void main(String[] args) {
+       int x= new int[1,2,3,4,5];
+          
+    }
+    
+}
--- day5.java
+++ day5.java
@@ -1,7 +1,12 @@
 public class day5 {
     public static void main(String[] args) {
-        char ch='\u0061' ;
-        System.out.println(ch);
+        // char ch='\u0061' ;
+        // System.out.println(ch);
+        //  int x=0B1111;
+        //  System.out.println(x);
+     double d=1_2_234_6.7_8_9 ;
+         System.out.println(d);
+         
     }
      
 }
