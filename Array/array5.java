ad152b933eb23dd45e91384aac7e023a2efdbdfd
erashu122 <kbitu8676@gmail.com>
2025-02-28 19:58:46 +0530

    initial commit

---
+++ Array/array5.java
@@ -0,0 +1,18 @@
+public class array5 {
+    public static void main(String[] args) {
+        int[] x=new int[6];
+        //System.out.println(x.length);
+        String s="Ashutosh";
+        //System.out.println(s.length); //C.E= cannot find symbol,symbol:   variable length
+        //location: variable s of type String
+        //System.out.println(s.length()); //8
+        
+        //in term of string Array
+        String[] d={"A","AA","AAA"};
+        System.out.println(d.length); //3
+        //System.out.println(d.length()); //connot find symbol
+        //System.out.println(d[0].length); //connot find symbol
+        System.out.println(d[0].length());//2
+    }
+    
+} 
