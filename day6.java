9a6f0c5af61a8583a2f136413e608c04f9d2f4d9
erashu122 <kbitu8676@gmail.com>
2025-07-07 11:03:09 +0530

    Initial commit

---
+++ day6.java
@@ -0,0 +1,10 @@
+class day6 {
+    public static void main(String[] args) {
+    
+        int a=12;
+        int b=10;
+        int c=5;
+        int sum=a+b+c;
+        System.out.println("The sum is: " + sum);
+    }
+}
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,6 @@
+public class day2 {
+    public static void main(String[] args) {
+     Boolean  b=true;
+       System.out.println(b);
+    } 
+}
