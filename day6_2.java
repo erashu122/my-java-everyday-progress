a5baa444c97bc64ef788a605ee6b40645365c79d
erashu122 <kbitu8676@gmail.com>
2025-07-07 12:38:41 +0530

    Initial commit

---
+++ day6_2.java
@@ -0,0 +1,19 @@
+public class day6_2 {
+    public static void main(String[] args) {
+        char ch = 'A';
+        char ch2 = 062;
+        System.out.println(ch);
+        // The second character is represented by its octal value   
+        System.out.println(ch2);
+        // The output will be the character corresponding to the octal value 062, which is 'B'
+        // This is a simple Java program that demonstrates character representation using both character literals and octal values  
+        ashu.main(args); // Call the mixed mode operation class
+    }
+}
+
+// mixed mode operation
+class ashu{
+    public static void main(String[] args) {
+        System.err.println(10+2+" "+"ashu"+"pandit"+"  "+(10+12));
+    }
+}
